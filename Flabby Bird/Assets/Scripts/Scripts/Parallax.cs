32df457ceef393e04c785e72e912acbbab0ea4dd
n1nikm <26nmalaviya@cpsd.us>
2025-03-11 20:39:02 -0400

    First commit
    my first commit with all assets

---
+++ Flabby Bird/Assets/Scripts/Scripts/Parallax.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MyParallax : MonoBehaviour
+{
+    public float animationSpeed = 0.1f;
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(animationSpeed * Time.deltaTime, 0);
+    }
+
+}
+ 
