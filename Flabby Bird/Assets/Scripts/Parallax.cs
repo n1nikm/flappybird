012c79d761b319396230efb680c8d901be13e3ad
n1nikm <26nmalaviya@cpsd.us>
2025-04-10 13:33:26 -0400

    Add files via upload

--- Flabby Bird/Assets/Scripts/Parallax.cs
+++ Flabby Bird/Assets/Scripts/Parallax.cs
@@ -1,6 +1,18 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Parallax
+public class Parallax : MonoBehaviour
 {
-    
+    public float animationSpeed = 1f;
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(animationSpeed * Time.deltaTime, 0);
+    }
+
 }
